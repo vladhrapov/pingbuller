3e42077302f3ec24acdd81040f6cd6c2014a447d
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-06-22 03:05:35 +0300

    Add scripts

--- dest/js/script.js
+++ dest/js/script.js
@@ -1,14 +1,29 @@
 window.onload = function () {
     $(function() {
 
-            $(".menu .menu__item:first-child").click(function () {
-                if($(document).width() < 768)
-                {
-                    $('.menu__item').toggle();
-                    $('.menu__item:first-child').toggle();
-                }
+        var isOpened = 0;
 
-            });
+        $(".menu .menu__item:first-child").click(function () {
+
+            if($(document).width() < 768)
+            {
+                isOpened++;
+                $('.menu__item').toggle();
+                $('.menu__item:first-child').toggle();
+                if(isOpened % 2 == 1)
+                {
+                    $('.menu__item:first-child').css('border-radius', '13px');
+                }
+                else
+                {
+                    $('.menu__item:first-child').css('border-radius', '13px 13px 0 0');
+                }
+            }
+
+        });
+
+
+            //$()
 
     });
 };
